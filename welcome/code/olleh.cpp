30dde63423b90d311af2d5ef2f0ef5c3da5c8c93
jacobfingerman <jacobfingerman@ucs.edu>
2022-10-02 17:07:05 -0700

    Added first homework

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -8,35 +8,44 @@ char* flipSent(string sent);
 int main()
 {
     string sentence;
+
     getline(std::cin, sentence);
     cout << flipSent(sentence) << "\n";
-
+    
     return 0;
 }
 
 char* flipSent(string sent) {
-    int start = -1;
+	int size = sent.size();
+    int start = -1; // Marks punctuation
 
     char* flipped = nullptr;
-    flipped = new char[sent.size()];
+    flipped = new char[size];
 
-    for (int i = 0; i < sent.size(); i++) {
+    for (int i = 0; i < size; i++) {
+        // Punctuation test
         if (ispunct(sent[i]) || isspace(sent[i])) {
 
-            if (start >= 0) flipped[start] = sent[start];
+            // Adds punctuation at old start point
+            if (start >= 0) flipped[start] = sent[start]; 
 
+            // Loops backwards from current to last punctuation, flipping word
             for (int j = i - 1; j > start; j--) {
                 flipped[start + (i - j)] = sent[j];
             }
 
-            start = i;
+            start = i; // Current punctuation becomes new start
 
-            if (i == sent.size() - 1) flipped[i] = sent[i];
+            // Ending in punctuation case
+            if (i == size - 1) flipped[i] = sent[i]; 
         }
-        else if (i == sent.size() - 1) {
+        // Ending in word character case
+        else if (i == size - 1) {
+            
+            // Adds last punctuation
+            if (start >= 0) flipped[start] = sent[start]; 
 
-            if (start >= 0) flipped[start] = sent[start];
-
+            // Loops backwards to last punctuation, flipping word
             for (int j = i; j > start; j--) {
                 flipped[start + 1 + (i - j)] = sent[j];
             }
